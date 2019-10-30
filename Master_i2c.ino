2c0366bb38f18f773b0f372016351556f4622d3e
eng-abdulhanan <57168597+eng-abdulhanan@users.noreply.github.com>
2019-10-31 01:12:49 +0500

    Add files via upload

---
+++ Master_i2c.ino
@@ -0,0 +1,19 @@
+#include<Wire.h>
+int x = 0;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(); 
+}
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  
+  Wire.requestFrom(9,1);
+  int ans = Wire.read();
+  Serial.println("Answer Received: ");
+  Serial.println(ans);
+  x++; 
+  if (x > 5) x = 0;
+  delay(500);
+}
---
+++ Slave_i2c.ino
@@ -0,0 +1,26 @@
+#include<Wire.h>
+int x = 0;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(9); 
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent); 
+}
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+void requestEvent()                                //This Function is called when Master wants value from slave
+{
+  int mul=2;
+  int S_ans=x*mul;
+  Wire.write(S_ans);                          // sends one byte converted POT value to master
+}
+
+void loop() {
+  if(x)
+  {
+  Serial.println("Value From Master: ");
+  Serial.println(x);
+  x=0;
+  }
+}
